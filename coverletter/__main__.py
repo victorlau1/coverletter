a9014dcf4bb60d2a84a3091dac76c5d746e1e9f6
Victor Lau <lau.victor.w@gmail.com>
2018-01-28 16:09:45 -0800

    get parse for input

--- coverletter/__main__.py
+++ coverletter/__main__.py
@@ -7,39 +7,80 @@ class CoverLetterParser(cmd.Cmd):
   """
     Initiates an Instance of Document Parser with Template
   """
-  
+  separators = {
+    1: '[]',
+    2: '{}',
+    3: '//'
+  }
+
+
   intro = "Simple Cover Letter Parser" 
 
   def do_prompt(self, line):
     #Present a link to a word document
-    directory = input('Please input the absolute path to the local document: ')
+    #directory = input('Please input the absolute path to the local document: ')
+    directory = '/mnt/c/Users/Victor/Downloads/Cover Letter Template.docx'
     file_template = open(directory, 'rb')
     document = Document(file_template)
-    self.parse_document(document)
+    separator = self.select_separator()
+    self.parse_document(document, separator)
 
   #Once Open Parse the Document for Separators, defaults to [];
   @classmethod
-  def parse_document(self, document):
+  def parse_document(self, document, separator):
     """
       Parse the document
     """
+
+    temp = separator[0]
+    tracking_text = False
+    counter = 0
+    print('Selector is:', separator)
     #Go through the document
+    
     for paragraph in document.paragraphs:
-      print(paragraph.text)
-    
-    
+      counter = 0
+      for text in paragraph.text:
+        counter += 1
+        if tracking_text and text != separator[1]:
+          text = ''
+        if text == separator[0]:
+          tracking_text = True
+          text = ''
+          temp = self.get_placeholder(paragraph.text, counter, separator[1])
+        elif text == separator[1]:
+          tracking_text = False
+          print('[',temp,']')
+          self.insert_overwrite(paragraph.text)
+          end_length = counter + len(raw_text)
+          print(paragraph.text[counter:end_length])
+
+
+  #Grab placeholder
+  @staticmethod
+  def get_placeholder(text, temp_cnt, separator):
+    full_text = False
+    temp_text = ''
+    while not full_text:
+      if text[temp_cnt] == separator:
+        return temp_text
+      else: 
+        temp_text += text[temp_cnt]
+        temp_cnt += 1
 
   #Reads Inputs for Specific Headers Supported are within the list  
-  @classmethod
-  def insert_overwrite(self, text):
+  @staticmethod
+  def insert_overwrite(text, ):
     """
       Overwrite the separators with expected input;
     """
-    print(text)
+    text_split = text.split(' '))
+    for word in text_split:
+      if word[0] == separator
     new_text = input('What new text do you want to put inside?')
 
-  @classmethod
-  def save_input_field(self, answer):
+  @staticmethod
+  def save_input_field(answer):
     """
       For same fields, save them to a database so we can reuse the same input
     """
@@ -51,10 +92,29 @@ class CoverLetterParser(cmd.Cmd):
 
   @staticmethod
   def write_document(document):
+    """
+      Save document based on user input save location
+    """
+
     path = input('Where would you like to save the document?')
     new_name = input('What is the filename... example [FName-LName-Date]')
     document.save(path + new_name)
-    
+
+  @classmethod
+  def select_separator(self):
+    """
+      Allow user to specify selector that they will be utilizing
+    """
+    selected_input = False
+
+    while not selected_input:
+      for key in self.separators:
+        print('Option ', key, ' :', self.separators[key])
+      selected_input = input('Select an option from above or insert custom one: ') 
+      if (self.separators[int(selected_input)]):
+        return self.separators[int(selected_input)]
+      else:
+        return selected_input
 
 if __name__ == '__main__':
   CoverLetterParser().cmdloop()
