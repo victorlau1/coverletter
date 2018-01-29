4fa7fb1a211da56f44d8d0fb18a734ca95f79835
Victor Lau <lau.victor.w@gmail.com>
2018-01-28 16:09:56 -0800

    scope out test functions

--- coverletter/test/test_main.py
+++ coverletter/test/test_main.py
@@ -1,3 +1,18 @@
-import unitTest from test
+import unitTest from TestCase
+import coverletter
 
+class TestCoverLetter(TestCase):
 
+  #Test that it re-runs until valid location
+
+  #Test that parser reads the right input
+
+  #Test that it overwrite function overwrites properly
+
+  #Test that replace text works
+  
+  #Test that file is saved
+
+  #Test that it saves to current working directory when location is not valid
+
+  #Test that it defaults to option 1 for separator
