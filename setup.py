9777ec149c7bcb48175a0555ffad738af874efcd
Victor Lau <lau.victor.w@gmail.com>
2018-02-04 16:35:07 -0800

    add function to find test

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
+import unittest
 
 def readme():
     with open('README.rst') as f:
         return f.read()
+
+def my_test_suite():
+      test_loader = unittest.TestLoader()
+      test_suite = test_loader.discover('tests', pattern='test_*.py')
+      return test_suite
 
 setup(name='coverletter',
       version='0.1',
@@ -13,8 +19,9 @@ setup(name='coverletter',
       license='MIT',
       packages=['coverletter'],
       install_requires=[
-            'python-docx'
+            'python-docx',
+            'textblob'
       ],
-      tests_suite='nose.collector',
+      test_suite='nose.collector',
       tests_require=['nose'],
       zip_safe=False)
