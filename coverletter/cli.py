d36405b6102835c7014b983529a13cb9d00886a7
Victor Lau <lau.victor.w@gmail.com>
2018-02-04 23:21:12 -0800

    close file useage properly with tests

--- coverletter/cli.py
+++ coverletter/cli.py
@@ -14,12 +14,12 @@ class Cli(cmd.Cmd):
       Prompts to initiate the prompt to create from a template
     """
     directory = input('Please input the absolute path to the local document: ')
-    file_template = open(directory, 'rb')
-    document = Document(file_template)
-    separator = CoverLetterParser.select_separator()
-    CoverLetterParser.parse_document(document, separator)
+    with open(directory, 'rb') as file_template:
+      document = Document(file_template)
+      separator = CoverLetterParser.select_separator()
+      CoverLetterParser.parse_document(document, separator)
 
-  def do_EOF(self, arg):
+  def do_EOF(self):
     return True
 
 if __name__ == '__main__':
