d35a92605a4fdab5cd5fcb7d4e6ce98de220b66a
Victor Lau <lau.victor.w@gmail.com>
2018-02-04 19:10:37 -0800

    add test data and cli change

--- coverletter/tests/test_cli.py
+++ coverletter/tests/test_cli.py
@@ -1,13 +1,31 @@
 from unittest import TestCase
 from unittest.mock import patch
 from coverletter.cli import Cli
+import os
 
 class TestCli(TestCase):
 
-  #Test that do_prompt command works
+  #Test Prompt Exists
+  def test_prompt_start(self):
+    with patch('coverletter.cli.Cli.do_prompt', return_value='Y'):
+      result = Cli().do_prompt()
+      self.assertEqual(result, 'Y')
+
+  #Integration Test 1
   def test_prompt(self):
     cli = Cli()
-    with patch('coverletter.cli.Cli.do_prompt', side_effect='/fake/path'):
+    file_path = os.path.dirname(__file__) + '/data/'
+    testfile_path = os.path.abspath(os.path.join(file_path, 'testDoc.docx'))
+    user_input = [
+      testfile_path,
+      1,
+      'Whatever',
+      'Whatever',
+      'Y',
+      file_path,
+      'testDocOutput.docx'
+    ]
+    with patch('builtins.input', side_effect=user_input):
       result = cli.do_prompt()
-    self.assertIsNotNone(result)
+    self.assertIsNone(result)
   
