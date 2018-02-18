2e0069c4d63cac249e64f65d801f96e875b80b78
Victor Lau <lau.victor.w@gmail.com>
2018-02-17 21:18:02 -0800

    add top nouns output

--- coverletter/description.py
+++ coverletter/description.py
@@ -5,29 +5,38 @@ class JobDescriptionParser:
   
   def __init__(self, filepath):
     self.parse_document(filepath)
+    self.output = self.top_nouns(self.document)
 
-  @classmethod
   def parse_document(self, filepath):
     """
       Parses text document so that all paragraphs are accessible
     """
     with open(filepath, 'rb') as file_template:
-      document = Document(file_template)
-      self.top_words(document)
-
-  @classmethod
-  def top_words(self, document):
+      self.document = Document(file_template)
+      
+  def top_nouns(self, document):
     """
       Returns the total document paragraphs
     """
-    concatenate = ''
-    for paragraph in document.paragraphs:
-      concatenate += ' ' + paragraph.text
+    
+    text_string = " ".join(paragraph.text for paragraph in document.paragraphs)
 
-    total_text = TextBlob(concatenate)
+    total_text = TextBlob(text_string)
     test_dict = total_text.np_counts
     sorted_dict = sorted(test_dict, key=test_dict.get, reverse=True)
-    print(sorted_dict)
+    return sorted_dict[0:9]
+
+  def top_words(self, document):
+    """
+      Return top single words written in the document
+    """
+    totalstring = " ".join(paragraph.text for paragraph in document.paragraphs)
+    word_text = TextBlob(totalstring)
+    word_dict = word_text.word_counts
+    sorted_dict = sorted(word_dict, key=word_dict.get, reverse=True)
+    return sorted_dict[0:9]
+
+  
 
 if __name__ == '__main__':
   JobDescriptionParser()
