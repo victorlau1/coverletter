5d921d465a978dfb16c96c75808ebde92595896d
Victor Lau <lau.victor.w@gmail.com>
2018-02-17 21:17:47 -0800

    add tests for top nouns

--- coverletter/tests/test_description.py
+++ coverletter/tests/test_description.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from unittest.mock import patch
+from docx import Document
 from coverletter.description import JobDescriptionParser
 import os
 
@@ -7,18 +8,27 @@ class TestDescriptionParser(TestCase):
 
     #Read testfile within data
     def setUp(self):
-      file_path = os.path.dirname(__file__) + '/data/'
-      self.testfile_path = os.path.abspath(os.path.join(file_path, 'jobDescription.docx'))
+      filepath = os.path.dirname(__file__) + '/data/'
+      self.testfilepath = os.path.abspath(os.path.join(filepath, 'jobDescription.docx'))  
       self.test_inst = JobDescriptionParser
+
+      with open(self.testfilepath, 'rb') as file_template:
+        self.document = Document(file_template)
 
     #Test that it parses the job description document
     def test_parse_document(self):
-      self.test_inst.parse_document(self.testfile_path)
+      filepath = self.testfilepath
+      description_parser = self.test_inst
+      description_parser.parse_document(description_parser, filepath)
       self.assertIsNotNone(self.test_inst.document)
     
     #Returns output of top words associated with job
-    def test_top_words(self):
-      assert True
+    def test_top_nouns(self):
+      description_parser = self.test_inst
+      test_document = self.document
+      output = description_parser.top_nouns(description_parser, self.document)
+      self.assertEqual('experience', output[0])
+      self.assertEqual('sql', output[1])
 
     #Returns top nouns for inserted text (based on analysis)
     def test_textblob_nouns(self):
